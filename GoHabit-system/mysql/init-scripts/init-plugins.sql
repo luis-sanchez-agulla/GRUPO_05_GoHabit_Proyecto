49800e9fa8d5a6add5e2520bee91cad23eb32e0c
jacobo-perez-manteiga <9301232@alumnos.ufv.es>
2026-02-24 18:17:37 +0100

    carpeta mysql

---
+++ GoHabit-system/mysql/init-scripts/init-plugins.sql
@@ -0,0 +1,18 @@
+INSTALL PLUGIN rpl_semi_sync_source SONAME 'semisync_source.so';
+SET GLOBAL rpl_semi_sync_source_enabled = ON;
+
+-- Configurar timeout (ms) - si la réplica no responde, vuelve a asíncrono
+SET GLOBAL rpl_semi_sync_source_timeout = 10000;  -- 10 segundos
+
+
+
+INSTALL PLUGIN rpl_semi_sync_replica SONAME 'semisync_replica.so';
+SET GLOBAL rpl_semi_sync_replica_enabled = ON;
+
+-- Reiniciar replicación para que tome efecto
+STOP REPLICA;
+START REPLICA;
+
+-- Plugin de autenticar contraseña
+INSTALL PLUGIN validate_password SONAME 'validate_password.so';
+
