8530c141ec2ce0bdb0c0d36341d7df45ca30169c
Daniel Minguito Alaminos <daniel.minguito05@gmail.com>
2026-02-28 10:23:25 +0100

    priemras paginas frontend

--- GoHabit-system/frontend/app.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('welcomeButton').addEventListener('click', async () => {
-    try {
-        const response = await fetch('http://localhost:8000/');
-        const data = await response.json();
-        document.getElementById('message').textContent = data.message;
-    } catch (error) {
-        document.getElementById('message').textContent = 'Error fetching message';
-    }
-});
